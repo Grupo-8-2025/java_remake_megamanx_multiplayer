12e3ec4af65b667cc73dc8371c84abffcbd01a1c
Davi Augusto Marçal Rodrigues <daviamrodrigues@gmail.com>
2025-09-09 18:19:53 -0300

    Comentario TelaInicial

--- core/src/main/java/com/tp2/megamanx/TelaInicial.java
+++ core/src/main/java/com/tp2/megamanx/TelaInicial.java
@@ -15,6 +15,7 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class TelaInicial implements Screen {
 
+    //Inicia variaveis do jogo
     private Texture imgFundoMegaManX;
     private Texture imgCarregando;
     private FitViewport portTela;
@@ -28,11 +29,12 @@ public class TelaInicial implements Screen {
     private boolean digitandoNome = true;
     private Jogo jogo;
 
-    
+    //Cria a tela inicial
     public TelaInicial(Jogo jogo){
         this.jogo = jogo;
     }
 
+    //Cria os elementos da tela inicial
     private void create() {
         fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("font.ttf"));
         fontParameter = new FreeTypeFontParameter();
@@ -40,11 +42,12 @@ public class TelaInicial implements Screen {
         font = fontGenerator.generateFont(fontParameter);
 
         spriteBatch = new SpriteBatch();
-        portTela = new FitViewport(800, 500);
+        portTela = new FitViewport(800, 500);//Define o tamanho da tela
         imgFundoMegaManX = new Texture("imagens/TelaInicial/Mega_Man_X_Logo.png");
         //imgCarregando = new Texture("winXPLoading.jpeg");
     }
 
+    // Desenha a tela inicial
     void desenharTelaInicial(BitmapFont font, SpriteBatch spriteBatch, Texture imgFundoMegaManX, float worldHeight) {
         spriteBatch.draw(imgFundoMegaManX, 0, worldHeight - 250, 450, 220);
         font.setColor(Color.SKY);
@@ -52,49 +55,74 @@ public class TelaInicial implements Screen {
         font.draw(spriteBatch, "MEGA MAN X", 10, worldHeight - 300);
         font.draw(spriteBatch, "APERTE ENTER PARA COMECAR", 10, worldHeight - 320);
         font.draw(spriteBatch, "Digite seu nome:", 10, worldHeight - 350);
+        // (digitandoNome ? "|" : "") faz com que o cursor (|), caso digitandoNome for verdade, pisque enquanto o jogador digita o nome
         font.draw(spriteBatch, nomeJogador + (digitandoNome ? "|" : ""), 10, worldHeight - 370);
     }
 
+    // Mostra a tela inicial
     @Override
     public void show (){
         create();
     }
 
+    // Desenha a tela
     private void draw() {
         ScreenUtils.clear(Color.BLACK);
         portTela.apply();
         Gdx.gl.glClearColor(0, 0, 0, 1);
-        spriteBatch.begin();
+        spriteBatch.begin(); // <-- Comeca a desenhar
 
         float worldWidth = portTela.getWorldWidth();
         float worldHeight = portTela.getWorldHeight();
 
+        /*--------------------------------------------------------------
+        *
+        * Verifica se o jogo ja colocou o nome e apertou enter
+        *                                   
+        *--------------------------------------------------------------
+        */
+
+        // Se apertar enter, para de desenhar a tela inicial
         if (!digitandoNome) {
-            jogo.setScreen(null);
-            
-        } else {
+            jogo.setScreen(null);    
+        } 
+        // Se nao, desenha a tela inicial
+        else {
             desenharTelaInicial(font, spriteBatch, imgFundoMegaManX, worldHeight);
         }
 
-        spriteBatch.end();
+        spriteBatch.end();// <-- Termina de desenhar
     }
 
+    /*--------------------------------------------------------------
+    *
+    * Funcao que verifica a digitacao do nome do jogador
+    *
+    *--------------------------------------------------------------
+    */
     private void verificaDigitacaoNome() {
+        // Verifica se o jogador esta digitando o nome
+        // Se o jogador está digitando o nome
         if (digitandoNome) {
+            // Verifica se alguma tecla de A a Z foi pressionada
             for (int i = 0; i < 26; i++) {
                 if (Gdx.input.isKeyJustPressed(com.badlogic.gdx.Input.Keys.A + i)) {
+                    // Adiciona a letra correspondente ao nome do jogador
                     nomeJogador += (char) ('A' + i);
                 }
             }
+            // Verifica se a tecla BACKSPACE foi pressionada e remove o último caractere do nome
             if (Gdx.input.isKeyJustPressed(com.badlogic.gdx.Input.Keys.BACKSPACE) && nomeJogador.length() > 0) {
-                nomeJogador = nomeJogador.substring(0, nomeJogador.length() - 1);
+            nomeJogador = nomeJogador.substring(0, nomeJogador.length() - 1);
             }
+            // Verifica se a tecla ENTER foi pressionada e o nome não está vazio, finalizando a digitação
             if (Gdx.input.isKeyJustPressed(com.badlogic.gdx.Input.Keys.ENTER) && nomeJogador.length() > 0) {
-                digitandoNome = false;
+            digitandoNome = false;
             }
         }
     }
 
+    // Renderiza a tela
     @Override
 	public void render (float delta){
         if (digitandoNome) {
@@ -103,6 +131,7 @@ public class TelaInicial implements Screen {
         draw();
     }
 
+    // Redimensiona a tela
 	public void resize(int width, int height) {
         portTela.update(width, height);
     }
@@ -113,6 +142,7 @@ public class TelaInicial implements Screen {
 
 	public void hide (){}
 
+    // Descarrega os recursos
 	@Override
     public void dispose() {
         spriteBatch.dispose();
