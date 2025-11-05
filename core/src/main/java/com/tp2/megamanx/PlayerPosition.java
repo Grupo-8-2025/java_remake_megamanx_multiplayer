b7a05d426b39bb572eb4c0a6e89bd1204fff0531
Matheus Augusto C. Silva <129077218+MatheusASilva@users.noreply.github.com>
2025-11-05 00:17:26 -0300

    Delete PlayerPosition class
    Removed the PlayerPosition class, which represented the player's position in the network.

--- core/src/main/java/com/tp2/megamanx/PlayerPosition.java
+++ core/src/main/java/com/tp2/megamanx/PlayerPosition.java
@@ -1,18 +1,1 @@
-package com.tp2.megamanx;
 
-
-/*
- * Classe simples para representar a posição do jogador na rede.
- */
-public class PlayerPosition {
-    public float x, y;
-    public int id; // 0 para player1, 1 para player2
-
-    public PlayerPosition() {}
-
-    public PlayerPosition(float x, float y, int id) {
-        this.x = x;
-        this.y = y;
-        this.id = id;
-    }
-}
